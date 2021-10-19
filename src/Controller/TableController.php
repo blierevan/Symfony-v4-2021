ffd4db650d80e261b7631b74d20701ca4c002151
BLIER Evan <blieeva@ndlp.fr>
2021-10-19 12:44:39 +0200

    news

---
+++ src/Controller/TableController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/table", name="table")
+*/
+class TableController extends AbstractController
+{
+    /**
+     * @Route("/print/{n}", name="table_print")
+     */
+    public function index(int $n): Response
+    {
+        return $this->render('table/index.html.twig', [
+            'controller_name' => 'TableController',
+            'n'=>$n,
+        ]);
+    }
+}
